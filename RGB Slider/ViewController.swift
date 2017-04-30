32c4f7e00105b58efcb131d8a196f81b3799e65d
Payman Bayat <payman1983@gmail.com>
2017-04-30 22:12:53 +0430

    Initial Commit

---
+++ RGB Slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Slider
+//
+//  Created by Payman Bayat on 2/10/1396 AP.
+//  Copyright © 1396 AP Payman Bayat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
