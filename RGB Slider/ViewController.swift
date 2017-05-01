e20763ce3c99591ea011dcec59114cb0d305dbd0
Payman Bayat <payman1983@gmail.com>
2017-05-01 14:39:15 +0430

    Applying UserDefaults.

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -18,12 +18,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        redSlider.value = defaults.float(forKey: "red")
+        greenSlider.value = defaults.float(forKey: "green")
+        blueSlider.value = defaults.float(forKey: "blue")
+        
         updateBackgroundColor()
         colorSquare.layer.borderColor = UIColor.black.cgColor
         colorSquare.layer.borderWidth = 1
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "openColor" {
+            let newViewControll = segue.destination
+            newViewControll.view.backgroundColor = colorSquare.backgroundColor
+        }
+    }
     @IBAction func updateBackgroundColor() {
         
         let red = CGFloat(redSlider.value)
@@ -31,6 +43,11 @@ class ViewController: UIViewController {
         let blue = CGFloat(blueSlider.value)
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
         
+        let defaults = UserDefaults.standard
+        defaults.set(redSlider.value, forKey: "red")
+        defaults.set(greenSlider.value, forKey: "green")
+        defaults.set(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
     }
     
 
