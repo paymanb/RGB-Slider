b8a35c1dd0aab76ad238301614ce7608cfa80c5c
Payman Bayat <payman1983@gmail.com>
2017-05-01 12:58:59 +0430

    This works

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -9,18 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
+        colorSquare.layer.borderColor = UIColor.black.cgColor
+        colorSquare.layer.borderWidth = 1
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func updateBackgroundColor() {
+        
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
     }
-
+    
 
 }
 
-//hhhhgggh
